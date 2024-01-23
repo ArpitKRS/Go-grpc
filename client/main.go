6bddfda1001e36640a4cc03271c3b1e1e6f96e0e
Arpit Kumar Singh <singharpit302@gmail.com>
2024-01-23 14:33:49 +0530

    Initial Setup

---
+++ client/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+
+	pb "github.com/ArpitKRS/go-grpc/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewGreetServiceClient(conn)
+
+	names := &pb.NamesList{
+		Names: []string{"Arpit", "Alice", "Bob"},
+	}
+
+	callSayHello(client)
+}
